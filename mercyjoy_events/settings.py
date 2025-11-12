e00b9f4a153791b2b4036545ee02868dcb638734
gitaguchu <gitaudennis59@gmail.com>
2025-11-12 17:04:01 +0300

    passenger files

--- mercyjoy_events/settings.py
+++ mercyjoy_events/settings.py
@@ -29,7 +29,8 @@ DEBUG = False
 ALLOWED_HOSTS = [
     '.vercel.app',
     '127.0.0.1',
-    '.now.sh'
+    '.now.sh',
+    'karibukenya.co.ke'
 ]
 
 
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,15 +1,9 @@
-import sys, os
-from pathlib import Path
+import os
+import sys
+from mercyjoy_events.wsgi import application
 
 # Path to your project directory
 BASE_DIR = Path(__file__).resolve().parent
 
 # Add project path to system path
 sys.path.insert(0, str(BASE_DIR))
-
-# Set the Django settings module
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mercyjoy_events.settings'  # <-- change this!
-
-# Import Django WSGI app
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
