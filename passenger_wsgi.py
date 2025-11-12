2c3c78c14e65d9c42a014e9203f048d0faa638bc
gitaguchu <gitaudennis59@gmail.com>
2025-11-12 12:20:05 +0300

    passenger.wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,15 @@
 import sys, os
-sys.path.insert(0, '/home3/karibuke/repositories/mercyjoy1')
+from pathlib import Path
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
+# Path to your project directory
+BASE_DIR = Path(__file__).resolve().parent
 
+# Add project path to system path
+sys.path.insert(0, str(BASE_DIR))
+
+# Set the Django settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mercyjoy_events.settings'  # <-- change this!
+
+# Import Django WSGI app
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
